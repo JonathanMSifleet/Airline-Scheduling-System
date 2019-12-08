3191fc1ca8763b75eb146a36bc5958ae09698427
JonathanMSifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2019-12-08 19:23:26 +0000

    Load passengers from database

--- src/solution/Main.java
+++ src/solution/Main.java
@@ -5,33 +5,42 @@ import java.nio.file.Paths;
 import baseclasses.DataLoadingException;
 import baseclasses.IAircraftDAO;
 import baseclasses.ICrewDAO;
+import baseclasses.IPassengerNumbersDAO;
 
 /**
- * This class allows you to run the code in your classes yourself, for testing and development
+ * This class allows you to run the code in your classes yourself, for testing
+ * and development
  */
 public class Main {
 
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
 		IAircraftDAO aircraft = new AircraftDAO();
-		
+
 		try {
 			aircraft.loadAircraftData(Paths.get("./data/aircraft.csv"));
-		}
-		catch (DataLoadingException dle) {
+		} catch (DataLoadingException dle) {
 			System.err.println("Error loading aircraft data");
 			dle.printStackTrace();
 		}
-		
+
 		ICrewDAO crew = new CrewDAO();
-		
+
 		try {
 			crew.loadCrewData(Paths.get("./data/crew.json"));
-		}
-		catch (DataLoadingException dle) {
+		} catch (DataLoadingException dle) {
 			System.err.println("Error loading crew data");
 			dle.printStackTrace();
 		}
-		
+
+		IPassengerNumbersDAO passengers = new PassengerNumbersDAO();
+
+		try {
+			passengers.loadPassengerNumbersData(Paths.get("./data/passengernumbers.db"));
+		} catch (DataLoadingException dle) {
+			System.err.println("Error loading passenger data");
+			dle.printStackTrace();
+		}
+
 	}
 
 }
--- src/solution/PassengerNumbersDAO.java
+++ src/solution/PassengerNumbersDAO.java
@@ -1,9 +1,13 @@
 package solution;
+
 import java.nio.file.Path;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 import baseclasses.DataLoadingException;
 import baseclasses.IPassengerNumbersDAO;
+import org.sqlite.*;
+import java.sql.*;
 
 /**
  * The PassengerNumbersDAO is responsible for loading an SQLite database
@@ -11,33 +15,14 @@ import baseclasses.IPassengerNumbersDAO;
  */
 public class PassengerNumbersDAO implements IPassengerNumbersDAO {
 
+	ArrayList<String> arrayOfPassengers = new ArrayList<>();
 
 	/**
-	 * Returns the number of passenger number entries in the cache
-	 * @return the number of passenger number entries in the cache
-	 */
-	@Override
-	public int getNumberOfEntries() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	/**
-	 * Returns the predicted number of passengers for a given flight on a given date, or -1 if no data available
-	 * @param flightNumber The flight number of the flight to check for
-	 * @param date the date of the flight to check for
-	 * @return the predicted number of passengers, or -1 if no data available
-	 */
-	@Override
-	public int getPassengerNumbersFor(int flightNumber, LocalDate date) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	/**
-	 * Loads the passenger numbers data from the specified SQLite database into a cache for future calls to getPassengerNumbersFor()
-	 * Multiple calls to this method are additive, but flight numbers/dates previously cached will be overwritten
-	 * The cache can be reset by calling reset() 
+	 * Loads the passenger numbers data from the specified SQLite database into a
+	 * cache for future calls to getPassengerNumbersFor() Multiple calls to this
+	 * method are additive, but flight numbers/dates previously cached will be
+	 * overwritten The cache can be reset by calling reset()
+	 * 
 	 * @param p The path of the SQLite database to load data from
 	 * @throws DataLoadingException If there is a problem loading from the database
 	 */
@@ -45,6 +30,57 @@ public class PassengerNumbersDAO implements IPassengerNumbersDAO {
 	public void loadPassengerNumbersData(Path p) throws DataLoadingException {
 		// TODO Auto-generated method stub
 
+		Connection c = null;
+		int size = 0;
+		try {
+			// connect to DB:
+			c = DriverManager.getConnection("jdbc:sqlite:" + p);
+
+			// run query:
+			Statement s = c.createStatement();
+			ResultSet rs = s.executeQuery("SELECT * FROM PassengerNumbers");
+
+			// fetch results:
+			while (rs.next()) {
+				size++;
+				String tempString = rs.getString("Date") + ",";
+				tempString = tempString + rs.getString("FlightNumber") + ",";
+				tempString = tempString + rs.getString("Passengers");
+				arrayOfPassengers.add(tempString);
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+		System.out.println(size);
+
+	}
+
+	/**
+	 * Returns the number of passenger number entries in the cache
+	 * 
+	 * @return the number of passenger number entries in the cache
+	 */
+	@Override
+	public int getNumberOfEntries() {
+		// TODO Auto-generated method stub
+		return arrayOfPassengers.size();
+	}
+
+	/**
+	 * Returns the predicted number of passengers for a given flight on a given
+	 * date, or -1 if no data available
+	 * 
+	 * @param flightNumber The flight number of the flight to check for
+	 * @param date the date of the flight to check for
+	 * @return the predicted number of passengers, or -1 if no data available
+	 */
+	@Override
+	public int getPassengerNumbersFor(int flightNumber, LocalDate date) {
+		// TODO Auto-generated method stub
+
+		// [do]
+
+		return 0;
 	}
 
 	/**
@@ -53,7 +89,7 @@ public class PassengerNumbersDAO implements IPassengerNumbersDAO {
 	@Override
 	public void reset() {
 		// TODO Auto-generated method stub
-
+		arrayOfPassengers = null;
 	}
 
 }

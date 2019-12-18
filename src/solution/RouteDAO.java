09dfb755373acb2d258d3ab75e0743449d2b7619
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2019-12-18 16:20:55 +0000

    Fix node data not loading correctly

--- src/solution/RouteDAO.java
+++ src/solution/RouteDAO.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,34 +41,38 @@ public class RouteDAO implements IRouteDAO {
 	public void loadRouteData(Path arg0) throws DataLoadingException {
 		// TODO Auto-generated method stub
 
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+
 		try {
 			// load the XML
 			DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 			Document doc = db.parse(arg0.toString());
 
 			Element root = doc.getDocumentElement();
-
 			NodeList routeNodes = root.getElementsByTagName("Route");
 
-			ArrayList<Object> routeData = new ArrayList<>();
 			for (int i = 0; i < routeNodes.getLength(); i++) {
-				routeData.add(routeNodes.item(i).getChildNodes().item(0).getNodeValue());
+
+				Route temp = new Route();
+				Node route = routeNodes.item(i);
+
+				Element routeData = (Element) route;
+
+				temp.setFlightNumber(Integer.parseInt(routeData.getElementsByTagName("FlightNumber").item(0).getTextContent()));
+				temp.setDayOfWeek(routeData.getElementsByTagName("DayOfWeek").item(0).getTextContent());
+				temp.setDepartureTime(LocalTime.parse(routeData.getElementsByTagName("DepartureTime").item(0).getTextContent(), formatter));
+				temp.setDepartureAirportCode(routeData.getElementsByTagName("DepartureAirportCode").item(0).getTextContent());
+				temp.setDepartureAirport(routeData.getElementsByTagName("DepartureAirport").item(0).getTextContent());
+				temp.setArrivalTime(LocalTime.parse(routeData.getElementsByTagName("ArrivalTime").item(0).getTextContent()));
+				temp.setArrivalAirport(routeData.getElementsByTagName("ArrivalAirport").item(0).getTextContent());
+				temp.setArrivalAirportCode(routeData.getElementsByTagName("ArrivalAirportCode").item(0).getTextContent());
+				temp.setDuration(java.time.Duration.parse(routeData.getElementsByTagName("Duration").item(0).getTextContent()));
+
+				System.out.println(temp.getFlightNumber());
+
+				// add route to array of routes
+				arrayOfRoutes.add(temp);
 			}
-
-			Route temp = new Route();
-			temp.setFlightNumber((int) routeData.get(0));
-			temp.setDayOfWeek((String) routeData.get(1));
-			temp.setDepartureTime((LocalTime) routeData.get(2));
-			temp.setDepartureAirport((String) routeData.get(3));
-			temp.setDepartureAirportCode((String) routeData.get(4));
-			temp.setArrivalTime((LocalTime) routeData.get(5));
-			temp.setArrivalAirport((String) routeData.get(6));
-			temp.setArrivalAirportCode((String) routeData.get(7));
-			temp.setDuration((Duration) routeData.get(8));
-
-			// add route to array of routes
-			arrayOfRoutes.add(temp);
-
 		} catch (ParserConfigurationException | SAXException | IOException e) {
 			System.err.println("Error opening XML file: " + e);
 		}
@@ -118,7 +123,7 @@ public class RouteDAO implements IRouteDAO {
 	 * Finds all of the flights that depart on the specified date
 	 * 
 	 * @param date the date to search for
-	 * @return A list of all routes that dpeart on this date
+	 * @return A list of all routes that depart on this date
 	 */
 	@Override
 	public List<Route> findRoutesbyDate(LocalDate date) {
